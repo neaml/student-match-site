9d08805e48cab8df13479be9ed5c1760e998bb15
neaml <neila95260@gmail.com>
2024-11-14 22:50:54 +0100

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// firebaseConfig.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDHWYEBdGMxoPTUGdV4wLqoTHmNFPrwMaE",
+  authDomain: "student-match-but3.firebaseapp.com",
+  projectId: "student-match-but3",
+  storageBucket: "student-match-but3.firebasestorage.app",
+  messagingSenderId: "1096075545413",
+  appId: "1:1096075545413:web:85451af9e19c5c54830c2c",
+  measurementId: "G-XK5N6FLSYJ"
+};
+
+export default firebaseConfig;
